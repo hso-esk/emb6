adf3d48a63bff791662027caadabebeab6237cfa
ayushev <artem.yushev@hs-offenburg.de>
2015-09-23 15:48:26 +0200

    Logger impovement

--- utils/inc/logger.h
+++ utils/inc/logger.h
@@ -73,6 +73,12 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <ctype.h>
+#include "bsp.h"
+
+#define IPV6ADDR_LEN    16
+#ifndef HEXDUMP_COLS
+#define HEXDUMP_COLS    8
+#endif
 
 /*!
 \brief   Print out IPv6 address
@@ -81,7 +87,35 @@
 
 \return   None
 */
-void log_ip6addr( const uint8_t* pc_addr);
+inline static void log_ip6addr( const uint8_t* pc_addr)
+{
+    uint16_t    i_addr_byte;
+    uint16_t    i;
+    uint32_t    f;
+    for( i = 0, f = 0; i < IPV6ADDR_LEN; i += 2 )
+    {
+        i_addr_byte = ( pc_addr[i] << 8 ) + pc_addr[i + 1];
+        if( i_addr_byte == 0 && f >= 0 )
+        {
+            if( f++ == 0 )
+            {
+                printf( "::" );
+            }
+        }
+        else
+        {
+            if( f > 0 )
+            {
+                f = -1;
+            }
+            else if( i > 0 )
+            {
+                printf( ":" );
+            }
+            printf( "%x", i_addr_byte );
+        }
+    }
+}
 
 /*!
 \brief   Printout data in a standard hex view
@@ -95,7 +129,56 @@ void log_ip6addr( const uint8_t* pc_addr);
 0x000008: 70 00 53 53 48 5f 41 47 p.SSH_AG
 0x000010: 45 4e 54 5f             ENT_
 */
-void log_hexdump(const void* p_buf, uint32_t l_len);
+/* We need a printable char, so it's not an warning or error
+ * "else if( isprint( ( (char*)p_buf )[j] ) )"
+ *  */
+#pragma GCC diagnostic ignored "-Wchar-subscripts"
+inline static void log_hexdump(const void* p_buf, uint32_t l_len)
+{
+    unsigned int i, j;
+
+    for( i = 0; i < l_len
+            + ( ( l_len % HEXDUMP_COLS ) ? ( HEXDUMP_COLS
+            - l_len % HEXDUMP_COLS ) : 0 ); i++ )
+    {
+        /* print offset */
+        if( i % HEXDUMP_COLS == 0 )
+        {
+            printf( "0x%06x: ", i );
+        }
+
+        /* print hex data */
+        if( i < l_len )
+        {
+            printf( "%02x ", 0xFF & ( (char*)p_buf )[i] );
+        }
+        else /* end of block, just aligning for ASCII dump */
+        {
+            printf( "   " );
+        }
+
+        /* print ASCII dump */
+        if( i % HEXDUMP_COLS == ( HEXDUMP_COLS - 1 ) )
+        {
+            for( j = i - ( HEXDUMP_COLS - 1 ); j <= i; j++ )
+            {
+                if( j >= l_len ) /* end of block, not really printing */
+                {
+                    putchar( ' ' );
+                }
+                else if( isprint( ( (char*)p_buf )[j] ) ) /* printable char */
+                {
+                    putchar( 0xFF & ( (char*)p_buf )[j] );
+                }
+                else /* other char */
+                {
+                    putchar( '.' );
+                }
+            }
+            putchar( '\n' );
+        }
+    }
+}
 
 
 #define LOG2_OK(msg, ...)       LOGGER_OK(2,msg, ##__VA_ARGS__)
@@ -106,7 +189,7 @@ void log_hexdump(const void* p_buf, uint32_t l_len);
 #define LOG2_DBG(msg, ...)      LOGGER_DBG(2,msg, ##__VA_ARGS__)
 #define LOG2_RAW(...)           LOGGER_RAW(2, __VA_ARGS__)
 #define LOG2_HEXDUMP(buf,len)   LOGGER_HEXDUMP(2, buf,len)
-#define LOG2_IP6ADDR(addr)     LOGGER_IP6ADDR(2, addr)
+#define LOG2_IP6ADDR(addr)      LOGGER_IP6ADDR(2, addr)
 
 #define LOG1_OK(msg, ...)       LOGGER_OK(1,msg, ##__VA_ARGS__)
 #define LOG1_ERR(msg, ...)      LOGGER_ERR(1,msg, ##__VA_ARGS__)
@@ -116,7 +199,7 @@ void log_hexdump(const void* p_buf, uint32_t l_len);
 #define LOG1_DBG(msg, ...)      LOGGER_DBG(1,msg, ##__VA_ARGS__)
 #define LOG1_RAW(...)           LOGGER_RAW(1, __VA_ARGS__)
 #define LOG1_HEXDUMP(buf,len)   LOGGER_HEXDUMP(1, buf,len)
-#define LOG1_IP6ADDR(addr)     LOGGER_IP6ADDR(1, addr)
+#define LOG1_IP6ADDR(addr)      LOGGER_IP6ADDR(1, addr)
 
 #define LOG_OK(msg, ...)        LOGGER_OK(0,msg, ##__VA_ARGS__)
 #define LOG_ERR(msg, ...)       LOGGER_ERR(0,msg, ##__VA_ARGS__)
@@ -126,7 +209,7 @@ void log_hexdump(const void* p_buf, uint32_t l_len);
 #define LOG_DBG(msg, ...)       LOGGER_DBG(0,msg, ##__VA_ARGS__)
 #define LOG_RAW(msg, ...)       LOGGER_RAW(0,msg, ##__VA_ARGS__)
 #define LOG_HEXDUMP(buf,len)    LOGGER_HEXDUMP(0, buf,len)
-#define LOG_IP6ADDR(addr)      LOGGER_IP6ADDR(0, (const uint8_t* )addr)
+#define LOG_IP6ADDR(addr)       LOGGER_IP6ADDR(0, (const uint8_t* )addr)
 
 #define LOGGER_OK(log_lvl, msg, ...)        \
     do { if ((LOGGER_ENABLE) && (LOGGER_LEVEL > log_lvl)) printf("%lu |   ok | %5s (%d)| " msg "\n", bsp_getSec(), __FILE__, __LINE__, ##__VA_ARGS__); }while (0)
