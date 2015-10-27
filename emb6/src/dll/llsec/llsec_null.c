196133bd17ee8b59476e88eeb0de51f2b2cac08b
Phuong Nguyen <mnguyen@hs-offenburg.de>
2015-10-27 15:15:28 +0100

    Restructured files of the netstack

---
+++ emb6/src/dll/llsec/llsec_null.c
@@ -0,0 +1,170 @@
+/*
+ * Copyright (c) 2013, Hasso-Plattner-Institut.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file is part of the Contiki operating system.
+ *
+ */
+
+/**
+ * \file
+ *         Insecure link layer security driver.
+ * \author
+ *         Konrad Krentz <konrad.krentz@gmail.com>
+ */
+
+/**
+ * \addtogroup llsec_null
+ * @{
+ */
+
+#include "emb6_conf.h"
+#include "emb6.h"
+#include "llsec_null.h"
+#include "framer_802154.h"
+#include "packetbuf.h"
+
+
+static s_ns_t          *LLSec_Netstk;
+static mac_callback_t   LLSec_TxCbFnct;
+static void            *LLSec_TxCbArg;
+
+/**
+ * @brief   Transmission callback function handler
+ *
+ * @param   p_arg
+ * @param   p_err
+ */
+static void LLSec_CbTx(void *p_arg, NETSTK_ERR *p_err)
+{
+    int status;
+    int transmission = 0;
+
+    switch (*p_err) {
+        case NETSTK_ERR_NONE:
+            status = MAC_TX_OK;
+            break;
+
+        case NETSTK_ERR_CHANNEL_ACESS_FAILURE:
+            status = MAC_TX_COLLISION;
+            break;
+
+        case NETSTK_ERR_TX_NOACK:
+            status = MAC_TX_NOACK;
+            break;
+
+        case NETSTK_ERR_BUSY:
+            status = MAC_TX_ERR;
+            break;
+
+        default:
+            status = MAC_TX_ERR_FATAL;
+            break;
+    }
+
+    LLSec_TxCbFnct(LLSec_TxCbArg, status, transmission);
+}
+
+
+/*---------------------------------------------------------------------------*/
+static void LLSec_Send(mac_callback_t sent, void *p_arg)
+{
+    NETSTK_ERR err = NETSTK_ERR_NONE;
+
+
+    LLSec_TxCbFnct = sent;
+    LLSec_TxCbArg = p_arg;
+    packetbuf_set_attr(PACKETBUF_ATTR_FRAME_TYPE, FRAME802154_DATAFRAME);
+
+
+    /*
+     * set TX callback function and argument
+     */
+    LLSec_Netstk->llc->ioctrl(NETSTK_CMD_TX_CBFNCT_SET,
+                             (void *)LLSec_CbTx,
+                             &err);
+
+    LLSec_Netstk->llc->ioctrl(NETSTK_CMD_TX_CBARG_SET,
+                             NULL,
+                             &err);
+
+    /*
+     * Issue next lower layer to transmit the prepared packet
+     */
+    LLSec_Netstk->llc->send(packetbuf_hdrptr(),
+                            packetbuf_totlen(),
+                            &err);
+}
+
+/*---------------------------------------------------------------------------*/
+static int LLSec_OnFrameCreated(void)
+{
+    return 1;
+}
+
+/*---------------------------------------------------------------------------*/
+static void LLSec_Input(void)
+{
+    LLSec_Netstk->hc->input();
+}
+
+/*---------------------------------------------------------------------------*/
+static uint8_t LLSec_GetOverhead(void)
+{
+    return 0;
+}
+
+/*---------------------------------------------------------------------------*/
+static void LLSec_Init(s_ns_t *p_netstk)
+{
+#if NETSTK_CFG_ARG_CHK_EN
+    if (p_netstk == NULL) {
+        return;
+    }
+#endif
+
+    NETSTK_ERR err = NETSTK_ERR_NONE;
+
+    LLSec_Netstk = p_netstk;
+    LLSec_Netstk->llc->ioctrl(NETSTK_CMD_RX_CBFNT_SET,
+                              (void *)LLSec_Input,
+                              &err);
+}
+
+/*---------------------------------------------------------------------------*/
+const s_nsllsec_t nullsec_driver =
+{
+       "LLSEC NULL",
+        LLSec_Init,
+        LLSec_Send,
+        LLSec_OnFrameCreated,
+        LLSec_Input,
+        LLSec_GetOverhead
+};
+/*---------------------------------------------------------------------------*/
+
+/** @} */
---
+++ emb6/src/phy/lpr/lpr_null.c
@@ -0,0 +1,221 @@
+/**
+ * @file    lpr_null.c
+ * @date    16.10.2015
+ * @author  PN
+ */
+
+
+/*
+********************************************************************************
+*                                   INCLUDES
+********************************************************************************
+*/
+#include "emb6_conf.h"
+
+
+#if NETSTK_CFG_LPR_NULL_EN
+/*
+********************************************************************************
+*                          LOCAL FUNCTION DECLARATIONS
+********************************************************************************
+*/
+static void LPR_Init(void *p_netstk, NETSTK_ERR *p_err);
+static void LPR_On(NETSTK_ERR *p_err);
+static void LPR_Off(NETSTK_ERR *p_err);
+static void LPR_Send(uint8_t *p_data, uint16_t len, NETSTK_ERR *p_err);
+static void LPR_Recv(uint8_t *p_data, uint16_t len, NETSTK_ERR *p_err);
+static void LPR_IOCtrl(NETSTK_IOC_CMD cmd, void *p_val, NETSTK_ERR *p_err);
+
+
+/*
+********************************************************************************
+*                               LOCAL VARIABLES
+********************************************************************************
+*/
+static s_ns_t       *LPR_Netstk;
+static void         *LPR_CbTxArg;
+static NETSTK_CBFNCT LPR_CbTxFnct;
+
+
+/*
+********************************************************************************
+*                               GLOBAL VARIABLES
+********************************************************************************
+*/
+NETSTK_MODULE_DRV LPRDrvNull =
+{
+   "LPR APSS",
+    LPR_Init,
+    LPR_On,
+    LPR_Off,
+    LPR_Send,
+    LPR_Recv,
+    LPR_IOCtrl
+};
+
+
+/*
+********************************************************************************
+*                           LOCAL FUNCTION DEFINITIONS
+********************************************************************************
+*/
+
+/**
+ * @brief   Initialize driver
+ *
+ * @param   p_netstk    Pointer to netstack structure
+ * @param   p_err       Pointer to a variable storing returned error code
+ */
+void LPR_Init(void *p_netstk, NETSTK_ERR *p_err)
+{
+#if NETSTK_CFG_ARG_CHK_EN
+    if (p_err == NULL) {
+        return;
+    }
+
+    if (p_netstk == NULL) {
+        *p_err = NETSTK_ERR_INVALID_ARGUMENT;
+        return;
+    }
+#endif
+
+
+    LPR_Netstk = p_netstk;
+    LPR_CbTxArg = NULL;
+    LPR_CbTxFnct = 0;
+    *p_err = NETSTK_ERR_NONE;
+}
+
+
+/**
+ * @brief   Turn driver on
+ *
+ * @param   p_err       Pointer to a variable storing returned error code
+ */
+void LPR_On(NETSTK_ERR *p_err)
+{
+#if NETSTK_CFG_ARG_CHK_EN
+    if (p_err == NULL) {
+        return;
+    }
+#endif
+
+
+    LPR_Netstk->rf->on(p_err);
+}
+
+
+/**
+ * @brief   Turn driver off
+ *
+ * @param   p_err       Pointer to a variable storing returned error code
+ */
+void LPR_Off(NETSTK_ERR *p_err)
+{
+#if NETSTK_CFG_ARG_CHK_EN
+    if (p_err == NULL) {
+        return;
+    }
+#endif
+
+
+    LPR_Netstk->rf->off(p_err);
+}
+
+
+/**
+ * @brief   Frame transmission handler
+ *
+ * @param   p_data      Pointer to buffer holding frame to send
+ * @param   len         Length of frame to send
+ * @param   p_err       Pointer to a variable storing returned error code
+ */
+void LPR_Send(uint8_t *p_data, uint16_t len, NETSTK_ERR *p_err)
+{
+#if NETSTK_CFG_ARG_CHK_EN
+    if (p_err == NULL) {
+        return;
+    }
+
+    if ((len == 0) ||
+        (p_data == NULL)) {
+        *p_err = NETSTK_ERR_INVALID_ARGUMENT;
+        return;
+    }
+#endif
+
+
+    LPR_Netstk->rf->send(p_data, len, p_err);
+    LPR_CbTxFnct(LPR_CbTxArg, p_err);
+}
+
+
+/**
+ * @brief   Frame reception handler
+ *
+ * @param   p_data      Pointer to buffer holding frame to receive
+ * @param   len         Length of frame to receive
+ * @param   p_err       Pointer to a variable storing returned error code
+ */
+void LPR_Recv(uint8_t *p_data, uint16_t len, NETSTK_ERR *p_err)
+{
+#if NETSTK_CFG_ARG_CHK_EN
+    if (p_err == NULL) {
+        return;
+    }
+
+    if ((len == 0) ||
+        (p_data == NULL)) {
+        *p_err = NETSTK_ERR_INVALID_ARGUMENT;
+        return;
+    }
+#endif
+
+
+    LPR_Netstk->phy->recv(p_data, len, p_err);
+}
+
+
+/**
+ * @brief    Miscellaneous commands handler
+ *
+ * @param   cmd         Command to be issued
+ * @param   p_val       Pointer to a variable related to the command
+ * @param   p_err       Pointer to a variable storing returned error code
+ */
+void LPR_IOCtrl(NETSTK_IOC_CMD cmd, void *p_val, NETSTK_ERR *p_err)
+{
+    *p_err = NETSTK_ERR_NONE;
+    switch (cmd) {
+        case NETSTK_CMD_TX_CBFNCT_SET:
+            if (p_val == NULL) {
+                *p_err = NETSTK_ERR_INVALID_ARGUMENT;
+            } else {
+                LPR_CbTxFnct = (NETSTK_CBFNCT)p_val;
+            }
+            break;
+
+        case NETSTK_CMD_TX_CBARG_SET:
+            LPR_CbTxArg = p_val;
+            break;
+
+        case NETSTK_CMD_PHY_XXX:
+            break;
+
+        case NETSTK_CMD_LPR_XXX:
+            *p_err = NETSTK_ERR_NONE;
+            break;
+
+        default:
+            LPR_Netstk->rf->ioctrl(cmd, p_val, p_err);
+            break;
+    }
+}
+
+
+/*
+********************************************************************************
+*                               END OF FILE
+********************************************************************************
+*/
+#endif /* NETSTK_CFG_LPR_NULL_EN */
