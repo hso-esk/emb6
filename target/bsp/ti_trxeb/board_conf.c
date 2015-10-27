61e1f7fe3b6ccaa7b14d7c1338a25f6209aebdef
Phuong Nguyen <mnguyen@hs-offenburg.de>
2015-10-27 15:39:05 +0100

    Updated configuration of TrxEB board for UDP Socket demo application

--- target/bsp/ti_trxeb/board_conf.c
+++ target/bsp/ti_trxeb/board_conf.c
@@ -6,14 +6,38 @@
  */
 
 
-#include "emb6.h"
-#include "bsp.h"
+#include "emb6_conf.h"
 #include "board_conf.h"
 
 
-uint8_t board_conf(s_ns_t* ps_nStack)
+
+/** Enable or disable logging */
+#define        LOGGER_ENABLE          LOGGER_BSP
+#include "logger.h"
+
+
+/**
+ * @brief   Configure Board Support Package module
+ *
+ * @param   ps_ns   Pointer to net stack structure
+ * @return  1 if success; otherwise 0
+ */
+uint8_t board_conf(s_ns_t* ps_ns)
 {
-    uint8_t c_ret = 0;
+    uint8_t     c_ret = 1;
+    NETSTK_ERR  err = NETSTK_ERR_NONE;
+
+
+    if (ps_ns != NULL) {
+        ps_ns->rf = &RFDrvCC1120;
+        ps_ns->rf->init(ps_ns, &err);
+        if (err != NETSTK_ERR_NONE) {
+            c_ret = 0;
+        }
+    }
+    else {
+        LOG_ERR("Network stack pointer is NULL");
+    }
 
     return c_ret;
 }
