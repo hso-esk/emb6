bf7faac1a1f7a98f7873ec4054ab57dc0e8b0b04
PhuongNguyen <PhuongNguyen>
2016-03-18 09:52:02 +0100

    When used, framer shall also set sequence number, expecially no-framer which is used by external interface (SLIP) app

--- emb6/src/net/sicslowpan/framer-802154.c
+++ emb6/src/net/sicslowpan/framer-802154.c
@@ -245,6 +245,7 @@ static int8_t parse(void)
     packetbuf_set_addr(PACKETBUF_ADDR_SENDER, (linkaddr_t *)&frame.src_addr);
     packetbuf_set_attr(PACKETBUF_ATTR_PENDING, frame.fcf.frame_pending);
     packetbuf_set_attr(PACKETBUF_ATTR_PACKET_ID, frame.seq);
+    frame802154_setDSN(frame.seq);
     if (frame.fcf.ack_required == 1) {
         packetbuf_set_attr(PACKETBUF_ATTR_RELIABLE, frame.fcf.ack_required);
     }
