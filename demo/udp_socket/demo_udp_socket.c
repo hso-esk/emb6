6da2a635be947d1413b26a8f5b1b05e8fe4e4470
PhuongNguyen <PhuongNguyen>
2016-04-12 11:30:02 +0200

    Refactored name convension of UDP socket demo application

--- demo/udp_socket/demo_udp_socket.c
+++ demo/udp_socket/demo_udp_socket.c
@@ -91,7 +91,7 @@
 #define DEF_UDP_REMPORT                 4211UL
 #endif
 
-#define DEF_UDP_SEND_INTERVAL           (clock_time_t)(1000u )
+#define DEF_UDP_SEND_INTERVAL           (clock_time_t)( 200u )
 
 #define UIP_IP_BUF                      ((struct uip_ip_hdr *)&uip_buf[UIP_LLH_LEN])
 
@@ -100,22 +100,22 @@
 *                               LOCAL VARIABLES
 ********************************************************************************
 */
-static struct uip_udp_conn *UDPSocket_Conn = (struct uip_udp_conn *) 0;
+static struct uip_udp_conn *pudp_socket_conn = (struct uip_udp_conn *) 0;
 
 #ifdef  DEMO_UDP_SOCKET_ROLE_CLIENT
-static uint32_t UDPSocket_CurrSeqTx;
-static uint32_t UDPSocket_LastSeqRx;
-static uint32_t UDPSocket_LostPktQty;
-static struct etimer UDPSocket_Etimer;
+static uint32_t udp_socket_currSeqTx;
+static uint32_t udp_socket_lastSeqRx;
+static uint32_t udp_socket_lostPktQty;
+static struct etimer udp_socket_etimer;
 #endif /* DEMO_UDP_SOCKET_ROLE_CLIENT */
 
 
 /*=============================================================================
  *                              LOCAL FUNCTION PROTOTYPE
  *============================================================================*/
-static void     UDPSocket_EventHandler(c_event_t c_event, p_data_t p_data);
-static void     UDPSocket_Tx(uint32_t seq);
-static uint32_t UDPSocket_GetSeq(uint8_t *p_data, uint16_t len);
+static void     udp_socket_eventHandler(c_event_t c_event, p_data_t p_data);
+static void     udp_socket_tx(uint32_t seq);
+static uint32_t udp_socket_getSeq(uint8_t *p_data, uint16_t len);
 
 
 /*
@@ -127,19 +127,17 @@ static uint32_t UDPSocket_GetSeq(uint8_t *p_data, uint16_t len);
 /**
  * @brief   Achieve sequence number of an UDP message
  */
-static uint32_t UDPSocket_GetSeq(uint8_t *p_data, uint16_t len)
+static uint32_t udp_socket_getSeq(uint8_t *p_data, uint16_t len)
 {
-    uint32_t ret = 0u;
-    uint8_t is_valid = 0u;
+  uint32_t ret = 0u;
+  uint8_t is_valid = FALSE;
 
+  is_valid = (p_data != NULL) && (len > 0);
+  if (is_valid == TRUE) {
+    memcpy(&ret, p_data, sizeof(ret));
+  }
 
-    is_valid = (p_data != NULL) &&
-               (len > 0);
-    if (is_valid == 1u) {
-        memcpy(&ret, p_data, sizeof(ret));
-    }
-
-    return ret;
+  return ret;
 }
 
 
@@ -148,65 +146,58 @@ static uint32_t UDPSocket_GetSeq(uint8_t *p_data, uint16_t len)
  *
  * @param   seq     Sequence number of the UDP message to transmit
  */
-static void UDPSocket_Tx(uint32_t seq)
+static void udp_socket_tx(uint32_t seq)
 {
-    uint8_t     seq_size = sizeof(seq);
-    uint8_t     payload[] = { 0, 0, 0, 0, 0x50, 0x51, 0x52, 0x53, 0x54 };
-    uint16_t    payload_len = sizeof(payload);
+  uint8_t seq_size = sizeof(seq);
+  uint8_t payload[] = { 0, 0, 0, 0, 0x50, 0x51, 0x52, 0x53, 0x54 };
+  uint16_t payload_len = sizeof(payload);
 
-
-    /*
-     * create packet to send
-     */
-    memcpy(payload, &seq, seq_size);
+  /*
+   * create packet to send
+   */
+  memcpy(payload, &seq, seq_size);
 
 #ifdef DEMO_UDP_SOCKET_ROLE_CLIENT
-    uip_ds6_addr_t  *ps_src_addr;
-    rpl_dag_t       *ps_dag_desc;
+  uip_ds6_addr_t *ps_src_addr;
+  rpl_dag_t *ps_dag_desc;
 
-    /*
-     * Get Server IP address
-     */
-    ps_src_addr = uip_ds6_get_global(ADDR_PREFERRED);
-    if (ps_src_addr) {
-        ps_dag_desc = rpl_get_any_dag();
-        if (ps_dag_desc) {
-            /*
-             * Issue transmission request
-             */
-            uip_ipaddr_copy(&UDPSocket_Conn->ripaddr, &ps_src_addr->ipaddr);
-            memcpy(&UDPSocket_Conn->ripaddr.u8[8], &ps_dag_desc->dag_id.u8[8], 8);
+  /*
+   * Get Server IP address
+   */
+  ps_src_addr = uip_ds6_get_global(ADDR_PREFERRED);
+  if (ps_src_addr) {
+    ps_dag_desc = rpl_get_any_dag();
+    if (ps_dag_desc) {
+      /*
+       * Issue transmission request
+       */
+      uip_ipaddr_copy(&pudp_socket_conn->ripaddr, &ps_src_addr->ipaddr);
+      memcpy(&pudp_socket_conn->ripaddr.u8[8], &ps_dag_desc->dag_id.u8[8], 8);
 
-            uip_udp_packet_send(UDPSocket_Conn, payload, payload_len);
-            uip_create_unspecified(&UDPSocket_Conn->ripaddr);
-        }
+      uip_udp_packet_send(pudp_socket_conn, payload, payload_len);
+      uip_create_unspecified(&pudp_socket_conn->ripaddr);
     }
+  }
 #endif
 
 
 #ifdef DEMO_UDP_SOCKET_ROLE_SERVER
-    uip_ipaddr_copy(&UDPSocket_Conn->ripaddr,
-                    &UIP_IP_BUF->srcipaddr);
-
-    uip_udp_packet_send(UDPSocket_Conn,
-                        payload,
-                        payload_len);
-
-    uip_create_unspecified(&UDPSocket_Conn->ripaddr);
+  uip_ipaddr_copy(&pudp_socket_conn->ripaddr, &UIP_IP_BUF->srcipaddr);
+  uip_udp_packet_send(pudp_socket_conn, payload, payload_len);
+  uip_create_unspecified(&pudp_socket_conn->ripaddr);
 #endif /* DEMO_UDP_SOCKET_ROLE_SERVER */
 
-
-    /*
-     * Logging
-     */
+  /*
+   * Logging
+   */
 #if LOGGER_ENABLE
-    LOG_RAW("UDP Sending...  : ");
-    uint16_t len = payload_len;
-    uint8_t *p_data = payload;
-    while (len--) {
-        LOG_RAW("%02x ", *p_data++);
-    }
-    LOG_RAW("\r\n");
+  LOG_RAW("UDP Sending...  : ");
+  uint16_t len = payload_len;
+  uint8_t *p_data = payload;
+  while (len--) {
+    LOG_RAW("%02x ", *p_data++);
+  }
+  LOG_RAW("\r\n");
 #endif
 }
 
@@ -218,66 +209,59 @@ static void UDPSocket_Tx(uint32_t seq)
  * @param   p_data      Callback argument that was registered as the event was
  *                      raised
  */
-static void UDPSocket_EventHandler(c_event_t c_event, p_data_t p_data)
+static void udp_socket_eventHandler(c_event_t c_event, p_data_t p_data)
 {
-    uint32_t    seq;
-    uint16_t    len;
-    uint8_t     has_data;
-    uint8_t    *p_dataptr;
+  uint32_t seq;
+  uint16_t len;
+  uint8_t  has_data;
+  uint8_t  *p_dataptr;
 
+  /*
+   * process input TCPIP packet
+   */
+  if (c_event == EVENT_TYPE_TCPIP) {
+    has_data = uip_newdata();
+    if (has_data != 0u) {
+      p_dataptr = (uint8_t *) uip_appdata;
+      len = (uint16_t) uip_datalen();
 
-    /*
-     * process input TCPIP packet
-     */
-    if (c_event == EVENT_TYPE_TCPIP) {
-        has_data = uip_newdata();
-        if (has_data != 0u) {
-            p_dataptr = (uint8_t *)uip_appdata;
-            len = (uint16_t)uip_datalen();
-
-            /* Obtain sequence number of the received message */
-            seq = UDPSocket_GetSeq(p_dataptr, len);
+      /* Obtain sequence number of the received message */
+      seq = udp_socket_getSeq(p_dataptr, len);
 
 #if LOGGER_ENABLE
-            /* Logging */
-            LOG_RAW("UDP Receiving...  : ");
-            while (len--) {
-                LOG_RAW("%02x ", *p_dataptr++);
-            }
-            LOG_RAW("\r\n");
+      /* Logging */
+      LOG_RAW("UDP Receiving...  : ");
+      while (len--) {
+        LOG_RAW("%02x ", *p_dataptr++);
+      }
+      LOG_RAW("\r\n");
 #endif
 
 #ifdef DEMO_UDP_SOCKET_ROLE_SERVER
-            //UDPSocket_Tx(seq);
+      //UDPSocket_Tx(seq);
 #else
-            /* TODO compare sequence number */
+      /* TODO compare sequence number */
 #endif /* DEMO_UDP_SOCKET_ROLE_SERVER */
-        }
     }
+  }
 #ifdef DEMO_UDP_SOCKET_ROLE_CLIENT
-    else {
-        int err = 0;
+  else {
+    int err = 0;
 
-        err = etimer_expired(&UDPSocket_Etimer);
-        if (err != 0) {
-            /*
-             * Restart event timer
-             */
-            etimer_restart(&UDPSocket_Etimer);
+    err = etimer_expired(&udp_socket_etimer);
+    if (err != 0) {
+      /* restart event timer */
+      etimer_restart(&udp_socket_etimer);
 
-            /*
-             * Sends a UDP message
-             */
-            UDPSocket_Tx(UDPSocket_CurrSeqTx);
+      /* sends a UDP message */
+      udp_socket_tx(udp_socket_currSeqTx);
 
-            /*
-             * Increase sequence number of UDP message
-             */
-            if (++UDPSocket_CurrSeqTx >= 1000) {
-                UDPSocket_CurrSeqTx = 0;
-            }
-        }
+      /* increase sequence number of UDP message */
+      if (++udp_socket_currSeqTx >= 1000) {
+        udp_socket_currSeqTx = 0;
+      }
     }
+  }
 #endif
 }
 
@@ -294,43 +278,37 @@ static void UDPSocket_EventHandler(c_event_t c_event, p_data_t p_data)
  */
 int8_t demo_udpSocketInit(void)
 {
-    /*
-     * Create a new UDP connection
-     */
-    UDPSocket_Conn = udp_new(NULL,
-                      UIP_HTONS(DEF_UDP_REMPORT),
-                      NULL);
+  /*
+   * Create a new UDP connection
+   */
+  pudp_socket_conn = udp_new(NULL, UIP_HTONS(DEF_UDP_REMPORT), NULL);
 
-    /*
-     * Bind the UDP connection to a local port
-     */
-    udp_bind(UDPSocket_Conn,
-             UIP_HTONS(DEF_UDP_DEVPORT));
+  /*
+   * Bind the UDP connection to a local port
+   */
+  udp_bind(pudp_socket_conn, UIP_HTONS(DEF_UDP_DEVPORT));
 
-    /* set callback for event process */
-    evproc_regCallback(EVENT_TYPE_TCPIP,
-                       UDPSocket_EventHandler);
+  /* set callback for event process */
+  evproc_regCallback(EVENT_TYPE_TCPIP, udp_socket_eventHandler);
 
 #ifdef DEMO_UDP_SOCKET_ROLE_CLIENT
-    clock_time_t interval = 0;
+  clock_time_t interval = 0;
 
-    /* set UDP event timer interval */
-    interval  = DEF_UDP_SEND_INTERVAL;
-    interval *= bsp_get(E_BSP_GET_TRES)/1000;
+  /* set UDP event timer interval */
+  interval  = DEF_UDP_SEND_INTERVAL;
+  interval *= bsp_get(E_BSP_GET_TRES)/1000;
 
-    /* Set event timer for periodic data process */
-    etimer_set(&UDPSocket_Etimer,
-                interval,
-                &UDPSocket_EventHandler);
+  /* Set event timer for periodic data process */
+  etimer_set(&udp_socket_etimer, interval, &udp_socket_eventHandler);
 
-    /* initialize statistic */
-    UDPSocket_CurrSeqTx = 0u;
-    UDPSocket_LastSeqRx = 0u;
-    UDPSocket_LostPktQty = 0u;
+  /* initialize statistic */
+  udp_socket_currSeqTx = 0u;
+  udp_socket_lastSeqRx = 0u;
+  udp_socket_lostPktQty = 0u;
 #endif
 
-    /* Always success */
-    return 1;
+  /* Always success */
+  return 1;
 }
 
 
