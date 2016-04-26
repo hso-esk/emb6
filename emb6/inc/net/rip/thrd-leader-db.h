f1a7c5e336bc54dabe727a94ae4c0099c31e5f3b
Lukas Zimmermann <lzimmer1@stud.hs-offenburg.de>
2016-04-26 14:59:57 +0200

    Adding leader database.

---
+++ emb6/inc/net/rip/thrd-leader-db.h
@@ -0,0 +1,52 @@
+/*
+ * thrd-leader-db.h
+ *
+ *  Created on: 26 Apr 2016
+ *  Author: Lukas Zimmermann <lzimmer1@stud.hs-offenburg.de>
+ *
+ *  Leader database manipulation.
+ */
+
+#ifndef EMB6_INC_NET_RIP_THRD_LEADER_DB_H_
+#define EMB6_INC_NET_RIP_THRD_LEADER_DB_H_
+
+#include "clist.h"
+#include "rip.h"
+
+void thrd_ldb_init(void);
+
+/**
+ * Routers acting as Leaders maintain a routing database for tracking Router ID
+ * assignments.
+ */
+
+/** \brief An 'ID Assignment Set' records the state of each Router ID.
+ * The leader database (ldb) is a set of thrd_ldb_id_assign_t structs. */
+typedef struct thrd_ldb_ida {
+	struct thrd_ldb_id *next;
+
+	uint8_t ID_id;			// A Router ID.
+	uint8_t ID_owner;		// The IEEE 802.15.4 Extended Address of the Router.
+	uint8_t ID_reuse_time;	// The time at which this Router ID MAY be reassigned.
+} thrd_ldb_ida_t;
+
+/** \name Routing Database basic routines */
+/** @{ */
+
+int thrd_ldb_num_ida(void);
+
+thrd_ldb_ida_t *thrd_ldb_ida_head(void);
+
+thrd_ldb_ida_t *thrd_ldb_ida_next(thrd_ldb_ida_t *i);
+
+thrd_ldb_ida_t *thrd_ldb_ida_lookup(uint8_t router_id);
+
+thrd_ldb_ida_t *thrd_ldb_ida_add(uint8_t router_id, uint8_t owner, uint8_t reuse_time);
+
+void thrd_ldb_ida_rm(thrd_ldb_ida_t *ida);
+
+#if RIP_DEBUG
+void thrd_ldb_print_leader_database(void);
+#endif /* RIP_DEBUG */
+
+#endif /* EMB6_INC_NET_RIP_THRD_LEADER_DB_H_ */
---
+++ emb6/src/net/rip/thrd-leader-db.c
@@ -0,0 +1,254 @@
+/*
+ * thrd-leader-db.c
+ *
+ *  Created on: 26 Apr 2016
+ *  Author: Lukas Zimmermann <lzimmer1@stud.hs-offenburg.de>
+ *
+ *  Leader database manipulation.
+ */
+
+#include "emb6.h"
+#include "stdlib.h"
+#include "clist.h"
+#include "memb.h"
+
+#include "thrd-leader-db.h"
+
+// #define DEBUG DEBUG_PRINT
+#include "uip-debug.h"
+
+/*
+ ********************************************************************************
+ *                          LOCAL FUNCTION DECLARATIONS
+ ********************************************************************************
+ */
+
+/*
+ ********************************************************************************
+ *                               LOCAL VARIABLES
+ ********************************************************************************
+ */
+
+/* Each 'ID Assignment Set' is represented by a thrd_ldb_id_assign structure
+ * and memory for each entry is allocated from the idassign_memb memory block.
+ * These routes are maintained on the idassign_list. */
+LIST(idassign_list);
+MEMB(idassign_memb, thrd_ldb_ida_t, MAX_ROUTERS); // TODO Check if MAX_ROUTERS is correct?
+
+/* Number of currently stored id assignments ids in the ID Assignment Set. */
+static int num_ida = 0;
+
+/*
+ ********************************************************************************
+ *                               GLOBAL VARIABLES
+ ********************************************************************************
+ */
+
+/*
+ ********************************************************************************
+ *                           LOCAL FUNCTION DEFINITIONS
+ ********************************************************************************
+ */
+
+/* --------------------------------------------------------------------------- */
+
+void
+thrd_ldb_init(void)
+{
+	memb_init(&idassign_memb);
+	list_init(idassign_list);
+}
+
+/* --------------------------------------------------------------------------- */
+
+int
+thrd_ldb_num_ida(void)
+{
+	return num_ida;
+}
+
+/* --------------------------------------------------------------------------- */
+
+thrd_ldb_ida_t
+*thrd_ldb_ida_head(void)
+{
+	return list_head(idassign_list);
+}
+
+/* --------------------------------------------------------------------------- */
+
+thrd_ldb_ida_t
+*thrd_ldb_ida_next(thrd_ldb_ida_t *i)
+{
+	if (i != NULL) {
+		thrd_ldb_ida_t *n = list_item_next(i);
+		return n;
+	}
+	return NULL;
+}
+
+/* --------------------------------------------------------------------------- */
+
+thrd_ldb_ida_t
+*thrd_ldb_ida_lookup(uint8_t router_id)
+{
+	thrd_ldb_ida_t *ida;
+	thrd_ldb_ida_t *found_ida;
+
+	PRINTF("thrd_ldb_ida_lookup: Looking up ID Assignment Set for router id ");
+	PRINTF("%d\n", router_id);
+	PRINTF("\n\r");
+
+	found_ida = NULL;
+	for (ida = thrd_ldb_ida_head(); ida != NULL; ida = thrd_ldb_ida_next(ida)) {
+		PRINTF("%d\n", ida->ID_id);
+		PRINTF("\n\r");
+		if (ida->ID_id == router_id) {
+			found_ida = ida;
+			break;
+		}
+	}
+
+	if (found_ida != NULL) {
+		PRINTF("thrd_ldb_ida_lookup: Found router id: ");
+		PRINTF("%d\n", router_id);
+		PRINTF("\n\r");
+	} else {
+		PRINTF("thrd_ldb_ida_lookup: No router id found\n\r");
+	}
+
+	if (found_ida != NULL && found_ida != list_head(idassign_list)) {
+		/* If we found the router id, we put it at the start of the idassign_list
+		 list. The list is ordered by how recently we looked them up:
+		 the least recently used router id will be at the end of the
+		 list - for fast lookups (assuming multiple packets to the same node). */
+		list_remove(idassign_list, found_ida);
+		list_push(idassign_list, found_ida);
+	}
+
+	return found_ida;
+}
+
+/* --------------------------------------------------------------------------- */
+
+thrd_ldb_ida_t
+*thrd_ldb_ida_add(uint8_t router_id, uint8_t owner, uint8_t reuse_time)
+{
+	thrd_ldb_ida_t *ida;
+
+	/* Find the corresponding Router ID entry (Router ID Set). */
+
+	ida = thrd_ldb_ida_lookup(router_id);
+
+	/* Check whether the given router id already has an entry in the Router ID Set. */
+	if (ida == NULL) {
+		PRINTF("thrd_ldb_ida_add: router id unknown for ");
+		PRINTF("%d\n", router_id);
+		PRINTF("\n\r");
+
+		/* If there is no router id entry, create one. We first need to
+		 * check if we have room for this router id. If not, we remove the
+		 * least recently used one we have. */
+
+		if (thrd_ldb_num_ida() == MAX_ROUTER_ID) {
+			/* Removing the oldest router id entry from the Router ID Set. The
+			 * least recently used link is the first router id on the set. */
+			thrd_ldb_ida_t *oldest;
+
+			oldest = list_tail(idassign_list);
+			thrd_ldb_ida_rm(oldest);
+		}
+
+		/* Allocate a router id entry and populate it. */
+		ida = memb_alloc(&idassign_memb);
+
+		if (ida == NULL) {
+			/* This should not happen, as we explicitly deallocated one
+			 * link set entry above. */
+			PRINTF("thrd_ldb_ida_add: could not allocate router id\n");
+			return NULL;
+		}
+
+		ida->ID_id = router_id;
+		ida->ID_owner = owner;
+		ida->ID_reuse_time = reuse_time;
+
+		/* Add new router id first - assuming that there is a reason to add this
+		 * and that there is a packet coming soon. */
+		list_push(idassign_list, ida);
+
+		PRINTF("thrd_ldb_ida_add: Added router id %d\n\r", router_id);
+
+		num_ida++;
+
+		PRINTF("thrd_ldb_ida_add: num_ida %d\n\r", num_ida);
+
+	} else {
+
+		PRINTF(
+				ANSI_COLOR_RED "thrd_ldb_ida_add: router id is already known for ");
+		PRINTF("%d\n", router_id);
+		PRINTF(ANSI_COLOR_RESET "\n\r");
+
+		PRINTF("thrd_ldb_idassign_add: num_ida %d\n\r", num_ida);
+		PRINTF("-----------------------------------------------------\n\r");
+
+		return NULL;
+	}
+
+	PRINTF("-----------------------------------------------------\n\r");
+
+	return ida;
+}
+
+/* --------------------------------------------------------------------------- */
+
+void
+thrd_ldb_ida_rm(thrd_ldb_ida_t *ida)
+{
+	if (ida != NULL) {
+		PRINTF("thrd_ldb_idassign_rm: removing router id from 'Router ID Set' with router id: ");
+		PRINTF("%d\n", ida->ID_id);
+		PRINTF("\n\r");
+
+		/* Remove the router id from the Router ID Set. */
+		list_remove(idassign_list, ida);
+		memb_free(&idassign_memb, ida);
+
+		num_ida--;
+
+		PRINTF("thrd_rdb_rid_rm: num_ida %d\n\r", num_ida);
+	}
+}
+
+/* --------------------------------------------------------------------------- */
+
+#if RIP_DEBUG
+void
+thrd_ldb_print_leader_database(void)
+{
+	thrd_ldb_ida_t *i;
+	printf(ANSI_COLOR_RED
+			"|============================== LEADER DATABASE ================================|"
+			ANSI_COLOR_RESET "\n\r");
+	printf("--------- ID ASSIGNMENT SET --------\n");
+	printf("| ID_id | ID_owner | ID_reuse_time |\n");
+	printf("------------------------------------\n\r");
+	for (i = thrd_ldb_ida_head(); i != NULL; i = thrd_ldb_ida_next(i)) {
+		printf("| " ANSI_COLOR_YELLOW "%5d" ANSI_COLOR_RESET
+				" | " ANSI_COLOR_YELLOW "%8d" ANSI_COLOR_RESET
+				" | " ANSI_COLOR_YELLOW "%13d" ANSI_COLOR_RESET
+				" |\n", i->ID_id, i->ID_owner, i->ID_reuse_time);
+	}
+	printf("------------------------------------\n\r");
+	printf(ANSI_COLOR_RED
+			"|===============================================================================|"
+			ANSI_COLOR_RESET "\n\r");
+}
+#endif /* RIP_DEBUG */
+
+/*
+ ********************************************************************************
+ *                               END OF FILE
+ ********************************************************************************
+ */
