51204802123f32f6c2a4b261373e968fad4d6988
Lukas Zimmermann <lzimmer1@stud.hs-offenburg.de>
2016-04-26 14:58:50 +0200

    Moving debug colors to rip.h

--- emb6/inc/net/rip/rip.h
+++ emb6/inc/net/rip/rip.h
@@ -8,6 +8,14 @@
 #ifndef EMB6_INC_NET_RIP_RIP_H_
 #define EMB6_INC_NET_RIP_RIP_H_
 
+/** Debug print colors. */
+#define ANSI_COLOR_RED     	"\x1b[31m"
+#define ANSI_COLOR_YELLOW  	"\x1b[33m"
+#define	ANSI_COLOR_GREEN	"\x1B[32m"
+#define ANSI_COLOR_CYAN    	"\x1b[36m"
+#define ANSI_COLOR_MAGENTA 	"\x1b[35m"
+#define ANSI_COLOR_RESET  	"\x1b[0m"
+
 /** Network Level Protocol Parameters */
 
 /** Routing table size */
