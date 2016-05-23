09c41683d303d08f6848932302073c2bcfd5515a
Lukas Zimmermann <lzimmer1@stud.hs-offenburg.de>
2016-05-23 14:25:54 +0200

    Adding thrd-send-adv.h header file.

---
+++ emb6/inc/net/rip/thrd-send-adv.h
@@ -0,0 +1,15 @@
+/*
+ * thrd-send-adv.h
+ *
+ *  Created on: 23 May 2016
+ *  Author: Lukas Zimmermann <lzimmer1@stud.hs-offenburg.de>
+ *
+ *  Sending Advertisements using Trickle.
+ */
+
+#ifndef EMB6_INC_NET_RIP_THRD_SEND_ADV_H_
+#define EMB6_INC_NET_RIP_THRD_SEND_ADV_H_
+
+extern void init(void);
+
+#endif /* EMB6_INC_NET_RIP_THRD_SEND_ADV_H_ */
