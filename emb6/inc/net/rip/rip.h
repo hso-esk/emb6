811557674f114f539e9ed920b47893bec84d1e2a
Lukas Zimmermann <lzimmer1@stud.hs-offenburg.de>
2016-05-23 14:26:51 +0200

    Sourcing out main defines to thread_conf.h

--- emb6/inc/net/rip/rip.h
+++ emb6/inc/net/rip/rip.h
@@ -16,40 +16,6 @@
 #define ANSI_COLOR_MAGENTA 	"\x1b[35m"
 #define ANSI_COLOR_RESET  	"\x1b[0m"
 
-/** Network Level Protocol Parameters */
-
-/** Routing table size */
-#define THRD_CONF_MAX_ROUTES            5 // 10
-
-/** Exponentially weighted moving average. */
-#define EXP_WEIGHT_MOV_AVG_1_8			3	// 1/8 (shifting bits).
-#define EXP_WEIGHT_MOV_AVG_1_16			4	// 1/16 (shifting bits).
-/** Set the weight. */
-#define THRD_EXP_WEIGHT_MOV_AVG			EXP_WEIGHT_MOV_AVG_1_8
-
-/** Network Layer defines. */
-#define	ADVERTISEMENT_I_MIN				1
-#define	ADVERTISEMENT_I_MAX				32
-#define	ID_REUSE_DELAY					100
-#define	ID_SEQUENCE_PERIOD				10
-#define	MAX_NEIGHBOR_AGE				100
-#define	MAX_ROUTE_COST					16
-#define	MAX_ROUTER_ID					5 // 62
-#define	MAX_ROUTERS						5 // 32
-#define	MIN_DOWNGRADE_NEIGHBORS			7
-#define	NETWORK_ID_TIMEOUT				120
-#define	PARENT_ROUTE_TO_LEADER_TIMEOUT	20
-#define	ROUTER_SELECTION_JITTER			120
-#define	ROUTER_DOWNGRADE_TRESHOLD		23
-#define	ROUTER_UPGRADE_TRESHOLD			16
-#define	INFINITE_COST_TIMEOUT			90
-#define	REED_ADVERTISEMENT_INTERVAL		570
-#define	REED_ADVERTISEMENT_MAX_JITTER	60
-
-/** Network Layer TLVs. */
-#define THRD_NET_TLV_MAX_NUM			10		// Maximum number of network layer TLVs.
-#define THRD_NET_TLV_MAX_SIZE			50		// Maximum size of network layer TLVs.
-
 // static uint8_t Router_ID;
 
 /** ------------------------- Structures ------------------------------- */
