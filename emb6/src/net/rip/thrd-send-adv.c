5cf0cc73e6b65d124f6cd61fb1f4a15f78b3bd0d
Lukas Zimmermann <lzimmer1@stud.hs-offenburg.de>
2016-05-24 12:22:33 +0200

    Adding 'Sending Advertisements' Trickle algorithm (full version).

--- emb6/src/net/rip/thrd-send-adv.c
+++ emb6/src/net/rip/thrd-send-adv.c
@@ -19,11 +19,12 @@
 
 /* Trickle Timers */
 struct trickle_param {
-	clock_time_t i_min;
-	clock_time_t i_max;			/* Max number of doublings. */
+	clock_time_t i_min;			/* The minimum interval size. */
+	uint8_t i_max;				/* Max number of doublings. */
 	clock_time_t t_start;		/* Start of the interval (absolute clock_time). */
 	clock_time_t t_end;			/* End of the interval (absolute clock_time). */
 	clock_time_t t_next;		/* Clock ticks, randomised in [I/2, I). */
+	clock_time_t interval;		/* I. */
 	uint8_t i_current;			/* Current doublings from i_min */
 	clock_time_t t_last_trigger;
 	uint8_t k;
@@ -46,7 +47,7 @@ struct trickle_param {
  * \brief Init trickle_timer[m]
  */
 #define TIMER_CONFIGURE() do { \
-		t.i_min = ADVERTISEMENT_I_MIN; \
+		t.i_min = ADVERTISEMENT_I_MIN * bsp_get(E_BSP_GET_TRES); \
 		t.i_max = ADVERTISEMENT_I_MAX; \
 		t.k = 0xFF; \
 } while(0)
@@ -55,120 +56,76 @@ struct trickle_param {
 /* Internal Data Structures */
 /*---------------------------------------------------------------------------*/
 static struct trickle_param t;
-static struct ctimer ct;
+static struct ctimer ci;		// Timer for interval I.
+static struct ctimer ct;		// Timer for time t.
 /*---------------------------------------------------------------------------*/
 /* Local function prototypes */
 /*---------------------------------------------------------------------------*/
 static void thrd_reset_trickle_timer();
-static void thrd_handle_timer(void *);
+static void thrd_handle_send_timer(void *);
 
 /*---------------------------------------------------------------------------*/
-/* Return a random number in [Imin, Imax]. */
+/* Return a random number in [min, d * min]. */
 static clock_time_t
-thrd_random_start_interval()
+thrd_random_start_interval(clock_time_t min, uint8_t d)
 {
-	// Random number between 1 and 32.
-	t.i_current = (random_rand() & 0x001F);
-	t.i_current = (t.i_current != 0) ? t.i_current : 1;
-
-	return (t.i_current * bsp_get(E_BSP_GET_TRES));
+	return ((random_rand() % (TRICKLE_TIME(min, d) - min + 1)) + min);
 }
 
 /*---------------------------------------------------------------------------*/
 /* Return a random number in [I/2, I). */
 static clock_time_t
-thrd_random_interval(clock_time_t i_min, uint8_t d)
+thrd_random_interval()
 {
-	clock_time_t min = TRICKLE_TIME(i_min >> 1, d);
-
-	PRINTF("THRD_SEND_ADV: Random [%lu, %lu)\n", (unsigned long) min,
-		 	(unsigned long)(TRICKLE_TIME(i_min, d)));
-
-	return min + (random_rand() % (TRICKLE_TIME(i_min, d) - 1 - min));
+	clock_time_t min = (t.interval >> 1);		// [I/2.
+	clock_time_t max = t.interval - 1;		// I).
+	// Be aware of 0.
+	if ( min == 0 )
+		min = 1;
+	if ( max == 0 )
+		max = 1;
+	return ((random_rand() % (max - min + 1)) + min);
 }
 
-/*---------------------------------------------------------------------------*/
-/* Called at the end of the current interval for timer ptr */
-static void
-thrd_double_interval(void *ptr)
-{
-	struct trickle_param *param = (struct trickle_param *)ptr;
-	int16_t offset;
-	clock_time_t next;
-
-	/*
-	 * If we got called long past our expiration, store the offset and try to
-	 * compensate this period
-	 */
-	offset = (int16_t)(bsp_getTick() - param->t_end);
-
-	/* Calculate next interval */
-	if(param->i_current < param->i_max) {
-		param->i_current++;
-	}
-
-	param->t_start = param->t_end;
-	param->t_end = param->t_start + (param->i_min << param->i_current);
-
-	next = thrd_random_interval(param->i_min, param->i_current);
-	if(next > offset) {
-		next -= offset;
-	} else {
-		next = 0;
-	}
-	param->t_next = next;
-	ctimer_set(&ct, param->t_next, thrd_handle_timer, (void *)param);
-
-	PRINTF("THRD_SEND_ADV: Doubling at %lu (offset %d), Start %lu, End %lu,"
-			" Periodic in %lu\n", bsp_getTick(), offset,
-			(unsigned long)param->t_start,
-			(unsigned long)param->t_end, (unsigned long)param->t_next);
-}
 
 /*---------------------------------------------------------------------------*/
 /*
- * Called at a random point in [I/2,I) of the current interval for ptr
- * PARAM is a pointer to the timer that triggered the callback (&t[index])
+ * Called after a random time in [I/2, I) has expired.
  */
 static void
-thrd_handle_timer(void *ptr)
+thrd_handle_send_timer(void *ptr)
 {
-	PRINTF("HELLO WORLD!\n");
+	PRINTF("thrd_handle_send_timer: Sending MLE Advertisement!\n");
 
-	struct trickle_param *param;
-	clock_time_t diff_last;       /* Time diff from last pass */
-	clock_time_t diff_start;      /* Time diff from interval start */
+	// TODO Send MLE Advertisement.
 
-	param = (struct trickle_param *)ptr;
-	if ( param == &t) {
+	return;
+}
 
-	} else {
-		/* This is an ooops and a serious one too */
-		return;
-	}
+/*---------------------------------------------------------------------------*/
+/* Called at the end of the current interval for timer. */
+static void
+thrd_double_interval(void *ptr)
+{
+	PRINTF("thrd_double_interval: Doubling interval!\n");
 
-	PRINTF("THRD_SEND_ADV: Periodic at %lu, last=%lu\n",
-			(unsigned long)bsp_getTick(),
-			(unsigned long)param->t_last_trigger);
+	t.interval = t.interval << 1;		// Double the interval I.
 
-	/* Temporarily store 'now' in t_next and calculate diffs */
-	param->t_next = bsp_getTick();
-	diff_last = param->t_next - param->t_last_trigger;
-	diff_start = param->t_next - param->t_start;
-	param->t_last_trigger = param->t_next;
+	PRINTF("thrd_double_interval: New interval: %lu secs!\n", (unsigned long)t.interval / bsp_get(E_BSP_GET_TRES));
 
-	PRINTF
-	("THRD_SEND_ADV: Periodic diff from last %lu, from start %lu\n",
-			(unsigned long) diff_last, (unsigned long) diff_start);
+	if ( t.interval > TRICKLE_TIME(t.i_min, t.i_max) )
+		t.interval = TRICKLE_TIME(t.i_min, t.i_max);
 
-	param->t_next = param->t_end - param->t_next;
+	ctimer_reset(&ct);
+
+	t.t_next = thrd_random_interval();
 
 	PRINTF
-	("THRD_SEND_ADV: Periodic at %lu, Interval End at %lu in %lu\n",
-			(unsigned long)bsp_getTick(), (unsigned long)param->t_end,
-			(unsigned long)param->t_next);
+	("thrd_handle_timer: Timer will expire in %lu secs.\n",
+			(unsigned long)t.t_next / bsp_get(E_BSP_GET_TRES));
 
-	ctimer_set(&ct, param->t_next, thrd_double_interval, (void *)param);
+	ctimer_set(&ci, t.interval, thrd_double_interval, (void *)&t);
+	ctimer_set(&ct, t.t_next, thrd_handle_send_timer, (void *)&t);
 
 	return;
 }
@@ -180,14 +137,19 @@ thrd_reset_trickle_timer()
 {
 	t.t_start = bsp_getTick();
 	t.t_end = t.t_start + (t.i_min);
-	t.i_current = 0;
-	t.t_next = thrd_random_start_interval();
+	t.interval = thrd_random_start_interval(t.i_min, t.i_max);	// I.
+	t.t_next = thrd_random_interval();	// t.
+
 
 	PRINTF
-	("thrd_reset_trickle_timer: Timer will expire in %lu secs.\n",
+	("thrd_reset_trickle_timer: Timer (I) will expire in %lu secs.\n",
+			(unsigned long)t.interval / bsp_get(E_BSP_GET_TRES));
+	PRINTF
+	("thrd_reset_trickle_timer: Timer (t) will expire in %lu secs.\n",
 			(unsigned long)t.t_next / bsp_get(E_BSP_GET_TRES));
 
-	ctimer_set(&ct, t.t_next, thrd_handle_timer, (void *)&t);
+	ctimer_set(&ci, t.interval, thrd_double_interval, (void *)&t);
+	ctimer_set(&ct, t.t_next, thrd_handle_send_timer, (void *)&t);
 }
 
 /*---------------------------------------------------------------------------*/
@@ -195,14 +157,6 @@ void
 init(void)
 {
 	PRINTF("THRD_SEND_ADV: Sending Advertisements.\n");
-
-	// memset(t, 0, sizeof(t));
-
-	// ROLL_TM_STATS_INIT();
-	// UIP_MCAST6_STATS_INIT(&stats);
-
-	// Register the ICMPv6 input handler.
-	// uip_icmp6_register_input_handler(&roll_tm_icmp_handler);
 
 	TIMER_CONFIGURE();
 
