269e70be8e860daa6c3eafcc06a8e866d6fb7d57
Lukas Zimmermann <lzimmer1@stud.hs-offenburg.de>
2016-05-24 17:54:52 +0200

    Adding a function to process Leader Data TLVs contained in a MLE Advertisement.

--- emb6/inc/net/rip/thrd-partition.h
+++ emb6/inc/net/rip/thrd-partition.h
@@ -10,6 +10,12 @@
 #ifndef EMB6_INC_NET_RIP_THRD_PARTITION_H_
 #define EMB6_INC_NET_RIP_THRD_PARTITION_H_
 
+#include "tlv.h"
+
+/*==============================================================================
+                          LOCAL VARIABLE DECLARATIONS
+ =============================================================================*/
+
 uint32_t Partition_ID;
 uint8_t VN_version;
 uint8_t VN_stable_version;
@@ -17,7 +23,15 @@ uint8_t ID_sequence_number;
 
 uint8_t Partition_weight;
 
+/*==============================================================================
+                                     API FUNCTIONS
+ =============================================================================*/
+
 void thrd_partition_start(void);
+
+void thrd_partition_process(uint8_t id_sequence_number, tlv_t *leader_tlv);
+
+void thrd_partition_empty(void);
 
 /*
  ********************************************************************************
--- emb6/src/net/rip/thrd-partition.c
+++ emb6/src/net/rip/thrd-partition.c
@@ -12,13 +12,30 @@
 #include "bsp.h"
 #include "thrd-partition.h"
 #include "rip.h"
+#include "tlv.h"
 
 #include "thrd-router-id.h"
 
 #define DEBUG DEBUG_PRINT
 #include "uip-debug.h"	// For debugging terminal output.
 
+/*==============================================================================
+                          LOCAL VARIABLE DECLARATIONS
+ =============================================================================*/
+
+/*==============================================================================
+                               LOCAL FUNCTION PROTOTYPES
+ =============================================================================*/
+
+/*==============================================================================
+                                    LOCAL FUNCTIONS
+ =============================================================================*/
+
 /* --------------------------------------------------------------------------- */
+
+/*==============================================================================
+                                     API FUNCTIONS
+ =============================================================================*/
 
 /**
  * Starting a new Partition.
@@ -40,11 +57,66 @@ thrd_partition_start(void)
 		ID_sequence_number = (uint8_t) bsp_getrand(0);
 		Partition_weight = 64;
 
-		thrd_print_partition_data();
+		// thrd_print_partition_data();
 	}
 }
 
 /* --------------------------------------------------------------------------- */
+
+/**
+ * Process a given Leader Data TLV and compare its content with the available
+ * leader data.
+ * @param id_sequence_number The corresponding ID Sequence Number (Route64 TLV).
+ * @param leader_tlv The Leader Data TLV.
+ */
+void
+thrd_partition_process(uint8_t id_sequence_number, tlv_t *leader_tlv)
+{
+	if ( (leader_tlv->type == TLV_LEADER_DATA) && (leader_tlv->length == 8) ) {
+		uint32_t partition_id = (leader_tlv->value[0] << 24)
+								| (leader_tlv->value[1] << 16)
+								| (leader_tlv->value[2] << 8)
+								| (leader_tlv->value[3]);
+		uint8_t weight = leader_tlv->value[4];
+		uint8_t data_version = leader_tlv->value[5];
+		uint8_t stable_data_version = leader_tlv->value[6];
+		uint8_t leader_router_id = leader_tlv->value[7];
+
+		PRINTF("TEST!\n");
+
+		// Compare partition values.
+		if ( weight > Partition_weight ) {
+			attach:
+			// TODO Try to attach to other partition.
+			PRINTF("thrd_partition_process: Try to attach to other partition!\n");
+		} else if ( weight == Partition_weight ) {
+			if ( partition_id > Partition_ID ) {
+				goto attach;
+			} else if ( partition_id == Partition_ID ) {
+				if ( (data_version > VN_version)
+						&& (stable_data_version > VN_stable_version)
+						&& (id_sequence_number > ID_sequence_number)) {
+					// Inconsistency detected -> Start a new Partition.
+					thrd_partition_start();
+				}
+			}
+		}
+		return;
+	}
+	PRINTF("thrd_partition_process: Invalid Leader Data TLV.\n");
+}
+
+/* --------------------------------------------------------------------------- */
+
+/**
+ * Remove all information related to the previous partition.
+ * Thread Spec.: Resetting Network Partition Data.
+ */
+void
+thrd_partition_empty(void)
+{
+	// TODO
+}
 
 /*
  ********************************************************************************
