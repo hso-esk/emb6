4af84356cb72e14ce352fbf0293fec2b692af9f6
Lukas Zimmermann <lzimmer1@stud.hs-offenburg.de>
2016-06-07 15:37:16 +0200

    Adding network layer TLV types (maybe not needed).

--- emb6/src/mle/tlv.c
+++ emb6/src/mle/tlv.c
@@ -4,6 +4,7 @@
  =============================================================================*/
 
 #include "tlv.h"
+#include "net_tlv.h"
 
 
 
@@ -53,6 +54,37 @@ int8_t tlv_connectivity_init(tlv_connectivity_t **tlv, uint8_t * ptr )
 	return 1 ;
 
 }
+
+// LZ. -------------------------------------------------------------------------
+
+int8_t tlv_target_eid_init(net_tlv_target_eid_t **tlv, uint8_t *ptr)
+{
+	if( ptr == NULL )
+		return 0 ;
+
+	*tlv= (net_tlv_target_eid_t*) ptr;
+	return 1 ;
+}
+
+int8_t tlv_rloc16_init(net_tlv_rloc16_t **tlv, uint8_t *ptr)
+{
+	if( ptr == NULL )
+		return 0 ;
+
+	*tlv= (net_tlv_rloc16_t*) ptr;
+	return 1 ;
+}
+
+int8_t tlv_ml_eid_init(net_tlv_ml_eid_t **tlv, uint8_t *ptr)
+{
+	if( ptr == NULL )
+		return 0 ;
+
+	*tlv= (net_tlv_ml_eid_t*) ptr;
+	return 1 ;
+}
+
+// -----------------------------------------------------------------------------
 
 
 int8_t tlv_write(tlv_t *tlv, tlv_type_t type, int8_t length, uint8_t * value )
