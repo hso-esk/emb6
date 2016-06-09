dc0d5ba0d2216f0b31a47506dd378ff864067e78
Lukas Zimmermann <lzimmer1@stud.hs-offenburg.de>
2016-06-09 10:13:00 +0200

    Implemenating exemplary POST handler function.

--- emb6/src/net/rip/resources/thrd-res-aq.c
+++ emb6/src/net/rip/resources/thrd-res-aq.c
@@ -10,7 +10,8 @@
 #include "bsp.h"
 #include "er-coap.h"
 
-#include "thrd-addr-query.h"
+#define DEBUG DEBUG_PRINT
+#include "uip-debug.h"
 
 /*
  ********************************************************************************
@@ -55,6 +56,24 @@ res_post_handler(void *request, void *response, uint8_t *buffer, uint16_t prefer
 	 * --> Call thrd_addr_ntf_response(uip_ipaddr_t *target_eid, uint16_t *rloc16,
 	 * 				uint8_t *ml_eid_tlv, clock_time_t *last_trans_time);
 	 */
+	printf("res_post_handler: Receiving CoAP packet!\n");
+
+	const uint8_t *chunk;
+	if ( !response ) {
+
+	} else {
+		int payload_len = coap_get_payload(request, &chunk);
+		if ( payload_len == 32 ) {
+			// TODO Process payload -> Receipt of Address Query Messages.
+		} else if ( payload_len == 38 ) {
+			// TODO Process payload -> Receipt of Address Query Messages.
+		}
+		printf("res_post_handler: payload_len = %d\n", payload_len);
+
+		for ( uint8_t i = 0; i < payload_len; i++ ) {
+			PRINTF("chunk[%d] = %02x\n", i, chunk[i]);
+		}
+	}
 }
 
 /* --------------------------------------------------------------------------- */
