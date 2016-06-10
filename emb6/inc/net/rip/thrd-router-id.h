9dbb881e9af82dc87c097e1291842769602cae2d
Lukas Zimmermann <lzimmer1@stud.hs-offenburg.de>
2016-06-10 16:52:56 +0200

    Modifying comments.

--- emb6/inc/net/rip/thrd-router-id.h
+++ emb6/inc/net/rip/thrd-router-id.h
@@ -11,6 +11,9 @@
 
 #include "emb6.h"
 
+/* Number of URIs that can be queried. */
+#define NUMBER_OF_URLS 2
+
 /*=============================================================================
                                Router ID Management
 ===============================================================================*/
@@ -23,6 +26,20 @@ clock_time_t thrd_next_period(uint8_t sec);
                                Router ID Assignment
 ===============================================================================*/
 
-extern void thrd_request_router_id(uint8_t router_id);
+/**
+ * Send an Address Solicit Request message to obtain a router id from the
+ * leader.
+ * @param leader_addr The Leader's IPv6 address.
+ * @param ml_eid The ML-EID.
+ * @param router_id The desired Router ID (optional).
+ */
+extern void thrd_request_router_id(uip_ipaddr_t *leader_addr, uint8_t *ml_eid,
+		uint8_t *router_id);
+
+/**
+ * Response handler for Address Solicit Request.
+ * @param response A pointer to the response.
+ */
+extern void thrd_addr_solicit_chunk_handler(void *response);
 
 #endif /* EMB6_INC_NET_RIP_THRD_ROUTER_ID_H_ */
--- emb6/src/net/rip/thrd-router-id.c
+++ emb6/src/net/rip/thrd-router-id.c
@@ -11,10 +11,12 @@
 #include "thread_conf.h"
 #include "ctimer.h"
 
+#include "er-coap.h"
+#include "er-coap-engine.h"
+#include "rest-engine.h"
+
 #include "thrd-router-id.h"
-
 #include "thrd-leader-db.h"
-
 #include "thrd-partition.h"
 
 #include "thrd-route.h"
@@ -26,16 +28,44 @@
                                Router ID Management
 ===============================================================================*/
 
+static char *service_urls[NUMBER_OF_URLS] =
+{ "a/as", "a/ar"};
+
 static struct ctimer leader_ct;
 // static struct ctimer router_ct;
 
-static void
-handle_timer(void *ptr)
-{
-	ID_sequence_number++;
-	thrd_print_partition_data();
-	ctimer_set(&leader_ct, (clock_time_t)thrd_next_period(ID_SEQUENCE_PERIOD), handle_timer, NULL);
-}
+/**
+ * Address Query Payload Buffer (MUST be at least 38 octets).
+ */
+static uint8_t addr_solicit_buf[24] = { 0 };
+
+static coap_packet_t packet[1]; /* This way the packet can be treated as pointer as usual. */
+
+static size_t len = 0;						// CoAP payload length.
+static tlv_t *tlv;
+static net_tlv_status_t *status_tlv;
+static net_tlv_rloc16_t *rloc16_tlv;
+static net_tlv_router_mask_t *router_mask_tlv;
+
+/*
+ * CoAP Resources to be activated need to be imported through the extern keyword.
+ */
+extern resource_t
+	thrd_res_a_as;
+
+/* --------------------------------------------------------------------------- */
+
+/**
+ * Initialize CoAP resources.
+ */
+static void coap_init();
+
+static void handle_timer(void *ptr);
+
+static size_t create_addr_solicit_req_payload(uint8_t *buf, uint8_t *ml_eid,
+		uint16_t *rloc16);
+
+/* --------------------------------------------------------------------------- */
 
 /**
  * The Leader maintains an ID_Assignment_Set containing the IEEE 802.15.4
@@ -49,15 +79,42 @@ thrd_leader_init(void)
 	// Starting a new Partition as the Leader.
 	thrd_dev.type = THRD_DEV_TYPE_LEADER;
 
+	coap_init();
+
 	thrd_ldb_ida_empty();	// Empty ID Assignment Set.
 	ctimer_set(&leader_ct, (clock_time_t)thrd_next_period(ID_SEQUENCE_PERIOD), handle_timer, NULL);
 }
+
+/* --------------------------------------------------------------------------- */
+
+static void
+coap_init()
+{
+	PRINTF("Router ID Assignment: Registering CoAP resources.\n\r");
+
+	// Bind the resources to their Uri-Path.
+	rest_activate_resource(&thrd_res_a_as, "a/as");
+}
+
+/* --------------------------------------------------------------------------- */
+
+static void
+handle_timer(void *ptr)
+{
+	ID_sequence_number++;
+	thrd_print_partition_data();
+	ctimer_set(&leader_ct, (clock_time_t)thrd_next_period(ID_SEQUENCE_PERIOD), handle_timer, NULL);
+}
+
+/* --------------------------------------------------------------------------- */
 
 clock_time_t
 thrd_next_period(uint8_t sec)
 {
 	return (clock_time_t)(sec * bsp_get(E_BSP_GET_TRES));
 }
+
+/* --------------------------------------------------------------------------- */
 
 void
 thrd_leader_dealloc_rid(uint8_t router_id)
@@ -79,14 +136,76 @@ thrd_leader_dealloc_rid(uint8_t router_id)
                                Router ID Assignment
 ===============================================================================*/
 
-/**
- * Send an Address Solicit Request message to obtain a router id from the
- * leader.
- * @param router_id The desired router id [0..62].
- */
 void
-thrd_request_router_id(uint8_t router_id)
+thrd_request_router_id(uip_ipaddr_t *leader_addr, uint8_t *ml_eid, uint8_t *router_id)
 {
+	uint16_t rloc16 = THRD_CREATE_RLOC16(*router_id, 0); // Create RLOC16.
+	len = create_addr_solicit_req_payload(addr_solicit_buf, ml_eid, &rloc16);
 
+	coap_init_message(packet, COAP_TYPE_CON, COAP_POST, 0);
+	coap_set_header_uri_path(packet, service_urls[0]);
+	coap_set_payload(packet, addr_solicit_buf, len);
+	coap_nonblocking_request(leader_addr, UIP_HTONS(COAP_DEFAULT_PORT), packet, thrd_addr_solicit_chunk_handler); // TODO Changing CoAP Port.
 }
+
+/* --------------------------------------------------------------------------- */
+
+static size_t
+create_addr_solicit_req_payload(uint8_t *buf, uint8_t *ml_eid, uint16_t *rloc16)
+{
+	if ( buf != NULL && ml_eid != NULL ) {
+		// Create ML-EID TLV.
+		buf[0] = NET_TLV_ML_EID;
+		buf[1] = 8;
+		memcpy(&buf[2], ml_eid, 8);
+		if ( rloc16 != NULL ) {
+			// Create RLOC16 TLV.
+			buf[10] = NET_TLV_RLOC16;
+			buf[11] = 2;
+			memcpy(&buf[12], rloc16, 2);
+			return 14;
+		}
+		return 10;
+	}
+	return 0;
+}
+
+/* --------------------------------------------------------------------------- */
+
+/* This function is will be passed to coap_nonblocking_request() to handle responses. */
+void
+thrd_addr_solicit_chunk_handler(void *response)
+{
+	PRINTF("thrd_addr_solicit_chunk_handler: Received response!");
+    const uint8_t *chunk;
+    if ( !response ) {
+
+    } else {
+    	int payload_len = coap_get_payload(response, &chunk);
+    	if ( payload_len >= 3 ) {
+    		// TODO Process payload -> Receipt of Address Solicit Response.
+    		tlv = (tlv_t*) &chunk[0];
+    		if ( tlv->type == NET_TLV_STATUS && tlv->length == 1 ) {
+    			status_tlv = (net_tlv_status_t*) tlv->value;
+    			PRINTF("Status = %d\n", status_tlv->status);
+    		}
+    		if ( status_tlv->status == 0 && payload_len == 16 ) {
+    			// Success.
+    			tlv = (tlv_t*) &chunk[3];
+    			if ( tlv->type == NET_TLV_RLOC16 && tlv->length == 2 ) {
+    				rloc16_tlv = (net_tlv_rloc16_t*) tlv->value;
+    				PRINTF("RLOC16 = %04x\n", rloc16_tlv->rloc16);
+    			}
+    			tlv = (tlv_t*) &chunk[7];
+    			if ( tlv->type == NET_TLV_ROUTER_MASK && tlv->length == 7 ) {
+    				router_mask_tlv = (net_tlv_router_mask_t*) tlv->value;
+    				PRINTF("ID Sequence Number = %d\n", router_mask_tlv->id_sequence_number);
+    				PRINTF("Router ID Mask = %16x\n", router_mask_tlv->router_id_mask);
+    			}
+    		}
+    	}
+    }
+}
+
+/* --------------------------------------------------------------------------- */
 
