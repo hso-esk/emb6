8c3b6b9766623fbcb129b6b0275cdbc6e9b8feb5
Lukas Zimmermann <lzimmer1@stud.hs-offenburg.de>
2016-06-15 15:56:53 +0200

    Adding functionality to create and modify Thread IPv6 addresses.

--- emb6/inc/net/rip/thrd-iface.h
+++ emb6/inc/net/rip/thrd-iface.h
@@ -2,12 +2,71 @@
  * thrd-iface.h
  *
  *  Created on: 15 Jun 2016
- *      Author: osboxes
+ *  Author: Lukas Zimmermann <lzimmer1@stud.hs-offenburg.de>
+ *  Thread Interface.
  */
 
 #ifndef EMB6_INC_NET_RIP_THRD_IFACE_H_
 #define EMB6_INC_NET_RIP_THRD_IFACE_H_
 
+#include "bsp.h"
+#include "uip.h"
 
+/**
+ * Create Mesh-Local Prefrix.
+ */
+#define thrd_create_meshlocal_prefix(addr) do { 	\
+		(addr)->u16[0] = UIP_HTONS(0xfdfd);         \
+		(addr)->u16[1] = 0x00;                      \
+		(addr)->u16[2] = 0x00;                      \
+		(addr)->u16[3] = 0x00;                      \
+} while(0)
+/**
+ * Create a random Interface Identifier (IID).
+ */
+#define thrd_create_random_iid(addr) do {			\
+		(addr)->u16[4] = bsp_getrand(0);			\
+		(addr)->u16[5] = bsp_getrand(0);			\
+		(addr)->u16[6] = bsp_getrand(0);			\
+		(addr)->u16[7] = bsp_getrand(0);			\
+} while(0)
+/**
+ * Check whether the given IPv6 address's IID is valid (Not allowed: 0000:00ff:fe00:xxxx).
+ */
+#define thrd_ml_eid_iid_is_valid(addr) (( (addr)->u16[4] == 0x0000 && (addr)->u16[5] == 0x00ff && (addr)->u16[6] == 0xfe00 ) ? FALSE : TRUE)
+/**
+ * Create IID of a given RLOC address.
+ */
+#define thrd_create_rloc_iid(addr, rloc16) do {		\
+		(addr)->u16[4] = UIP_HTONS(0x0000);			\
+		(addr)->u16[5] = UIP_HTONS(0x00ff);			\
+		(addr)->u16[6] = UIP_HTONS(0xfe00);			\
+		(addr)->u16[7] = UIP_HTONS(rloc16);			\
+} while(0)
+
+typedef struct {
+	uint16_t rloc16;
+
+	uip_ipaddr_t ml_eid;
+	uip_ipaddr_t ml_rloc;
+	uip_ipaddr_t ll_eid;
+	uip_ipaddr_t ll_rloc;
+
+#if ( (THRD_DEV_TYPE == THRD_DEV_TYPE_ROUTER) || (THRD_DEV_TYPE == THRD_DEV_TYPE_REED) )
+	uint8_t router_id;	// Router ID.
+#endif
+} thrd_iface_t;
+
+extern thrd_iface_t thrd_iface;
+
+void thrd_iface_init();
+
+/**
+ * Set the interface's RLOC16 and update the ML-RLOC and LL-RLOC addresses.
+ * @param rloc16 The RLOC16.
+ */
+void thrd_iface_rloc_set(uint16_t *rloc16);
+
+void thrd_iface_print();
 
 #endif /* EMB6_INC_NET_RIP_THRD_IFACE_H_ */
--- emb6/src/net/rip/thrd-iface.c
+++ emb6/src/net/rip/thrd-iface.c
@@ -2,7 +2,102 @@
  * thrd-iface.c
  *
  *  Created on: 15 Jun 2016
- *      Author: osboxes
+ *  Author: Lukas Zimmermann <lzimmer1@stud.hs-offenburg.de>
+ *  Thread Interface.
  */
 
+#include "thrd-iface.h"
+
+#define DEBUG DEBUG_PRINT
+#include "uip-debug.h"	// For debugging terminal output.
+
+/** Thread Network Partition. */
+thrd_iface_t thrd_iface;
+
+/*==============================================================================
+                          LOCAL VARIABLE DECLARATIONS
+ =============================================================================*/
+
+/*==============================================================================
+                               LOCAL FUNCTION PROTOTYPES
+ =============================================================================*/
+
+/*==============================================================================
+                                    LOCAL FUNCTIONS
+ =============================================================================*/
+
+/* --------------------------------------------------------------------------- */
+
+/*==============================================================================
+                                     API FUNCTIONS
+ =============================================================================*/
+
+void
+thrd_iface_init()
+{
+	// Create ML-EID.
+	uip_ipaddr_t addr;
+	thrd_create_meshlocal_prefix(&addr);
+	thrd_create_random_iid(&addr);
+	// Check whether the generated ML-EID's IID is valid.
+	while ( thrd_ml_eid_iid_is_valid(&addr) == FALSE ) {
+		thrd_create_random_iid(&addr);
+	}
+	thrd_iface.ml_eid = addr;
+	PRINTF("--------------\n ml_eid = ");
+	PRINT6ADDR(&addr);
+	PRINTF("\n ----------------\n");
+	// Create ML-RLOC.
+	thrd_create_rloc_iid(&addr, thrd_iface.rloc16);
+	thrd_iface.ml_rloc = addr;
+	// Create LL-IPv6 ADDRESS based on the MAX Extended Address.
+	// TODO
+	// Create LL-RLOC
+	uip_create_linklocal_prefix(&addr);
+	thrd_create_rloc_iid(&addr, thrd_iface.rloc16);
+	thrd_iface.ll_rloc = addr;
+	thrd_iface_print();
+}
+
+/* --------------------------------------------------------------------------- */
+
+void
+thrd_iface_rloc_set(uint16_t *rloc16)
+{
+	if ( thrd_iface.rloc16 != *rloc16 ) {
+		thrd_iface.rloc16 = *rloc16;
+		uip_ipaddr_t rloc;
+		// ML-RLOC.
+		thrd_create_meshlocal_prefix(&rloc);
+		thrd_create_rloc_iid(&rloc, *rloc16);
+		thrd_iface.ml_rloc = rloc;
+		// LL-RLOC.
+		uip_create_linklocal_prefix(&rloc);
+		thrd_create_rloc_iid(&rloc, *rloc16);
+		thrd_iface.ll_rloc = rloc;
+	}
+}
+
+/*==============================================================================
+                                    DEBUG FUNCTIONS
+ =============================================================================*/
+
+void
+thrd_iface_print()
+{
+	PRINTF(ANSI_COLOR_CYAN "|================================== THREAD INTERFACE ===================================|" ANSI_COLOR_RESET "\n\r");
+	PRINTF("ML-EID = ");
+	PRINT6ADDR(&thrd_iface.ml_eid);
+	PRINTF("\n");
+	PRINTF("ML-RLOC = ");
+	PRINT6ADDR(&thrd_iface.ml_rloc);
+	PRINTF("\n");
+	PRINTF("LL-EID = ");
+	PRINT6ADDR(&thrd_iface.ll_eid);
+	PRINTF("\n");
+	PRINTF("LL-RLOC = ");
+	PRINT6ADDR(&thrd_iface.ll_rloc);
+	PRINTF("\n");
+	PRINTF(ANSI_COLOR_CYAN "=========================================================================================" ANSI_COLOR_RESET "\n\r");
+}
 
