00e5268297f14400e60cf0462bab463a90df3b08
Lukas Zimmermann <lzimmer1@stud.hs-offenburg.de>
2016-07-11 14:57:43 +0200

    Adding functionality to manipulate Network Data sets.

--- emb6/src/net/rip/thrd-network-data.c
+++ emb6/src/net/rip/thrd-network-data.c
@@ -55,8 +55,17 @@ static thrd_server_set_t* thrd_server_set_lookup(uint16_t enterprise_number);
 static thrd_on_mesh_prefix_set_t* thrd_on_mesh_prefix_set_add(uint8_t *prefix, uint8_t domain_id,
 		uint16_t border_router_16, bool stable, bool slaac_preferred, bool slaac_valid, bool dhcp,
 		bool configure, bool def, bool preference);
+static thrd_ext_route_set_t* thrd_ext_route_set_add(uint8_t domain_id, uint16_t border_router_16,
+		uint8_t *prefix, bool stable, uint8_t preference);
+static thrd_sicslowpan_ctx_id_set_t* thrd_sicslowpan_ctx_id_set_add(uint8_t id, uint8_t *prefix,
+		bool compress, bool stable);
+static thrd_server_set_t* thrd_server_set_add(uint16_t enterprise_number, uint8_t *service_data,
+		uint16_t server_16, uint8_t *server_data, bool stable, uint8_t id);
 
 static void thrd_on_mesh_prefix_set_rm(thrd_on_mesh_prefix_set_t *pref);
+static void thrd_ext_route_set_rm(thrd_ext_route_set_t *ext_route);
+static void thrd_sicslowpan_ctx_id_set_rm(thrd_sicslowpan_ctx_id_set_t *ctx_id);
+static void thrd_server_set_rm(thrd_server_set_t *server);
 
 /*
  ********************************************************************************
@@ -86,7 +95,7 @@ MEMB(sicslowpan_ctx_id_memb, thrd_sicslowpan_ctx_id_set_t, THRD_MAX_SICSLOWPAN_C
  * Server Set memory allocation.
  */
 LIST(server_list);
-MEMB(server_memb, thrd_server_set_t, THRD_MAX_SERVER_SET_ENTIES);
+MEMB(server_memb, thrd_server_set_t, THRD_MAX_SERVER_SET_ENTRIES);
 
 /* Number of currently stored On-Mesh Prefixes in the On-Mesh Prefix Set. */
 static uint8_t num_on_mesh_prefixes = 0;
@@ -400,13 +409,161 @@ thrd_on_mesh_prefix_set_add(uint8_t *prefix, uint8_t domain_id,
 
 /* --------------------------------------------------------------------------- */
 
+static thrd_ext_route_set_t*
+thrd_ext_route_set_add(uint8_t domain_id, uint16_t border_router_16,
+		uint8_t *prefix, bool stable, uint8_t preference)
+{
+	thrd_ext_route_set_t *ext_route;
+	ext_route = thrd_ext_route_set_lookup(domain_id);
+
+	if ( ext_route == NULL ) {
+		PRINTF("thrd_ext_route_set_add: External Route unknown.\n\r");
+
+		if ( thrd_get_num_ext_routes() == THRD_MAX_EXT_ROUTE_SET_ENTRIES ) {
+			/* Removing the last entry from the External Route Set. */
+			thrd_ext_route_set_t *last;
+
+			last = list_tail(ext_route_list);
+			thrd_ext_route_set_rm(last);
+		}
+
+		/* Allocate a router id entry and populate it. */
+		ext_route = memb_alloc(&ext_route_memb);
+
+		if ( ext_route == NULL ) {
+			/* This should not happen, as we explicitly deallocated one
+			 * link set entry above. */
+			PRINTF("thrd_ext_route_set_add: Could not allocate External Route.\n\r");
+			return NULL;
+		}
+		list_push(ext_route_list, ext_route);
+
+		ext_route->R_domain_id = domain_id;
+		ext_route->R_border_router_16 = border_router_16;
+		memcpy(&(ext_route->R_prefix[0]), prefix, 8);
+		ext_route->R_stable = stable;
+		ext_route->R_preference = preference;
+		PRINTF("thrd_ext_route_set_add: Added External Route.\n\r");
+		num_ext_routes++;
+		PRINTF("thrd_ext_route_set_add: num_ext_routes %d\n\r", num_ext_routes);
+	} else {
+
+		PRINTF(ANSI_COLOR_RED "thrd_ext_route_set_add: External Route is already known.\n\r");
+		PRINTF("thrd_ext_route_set_add: num_ext_routes %d\n\r", num_ext_routes);
+		PRINTF("-----------------------------------------------------\n\r");
+		return NULL;
+	}
+	PRINTF("-----------------------------------------------------\n\r");
+	return ext_route;
+}
+
+/* --------------------------------------------------------------------------- */
+
+static thrd_sicslowpan_ctx_id_set_t*
+thrd_sicslowpan_ctx_id_set_add(uint8_t id, uint8_t *prefix, bool compress,
+		bool stable)
+{
+	thrd_sicslowpan_ctx_id_set_t *ctx_id;
+	ctx_id = thrd_sicslowpan_ctx_id_set_lookup(id);
+
+	if ( ctx_id == NULL ) {
+		PRINTF("thrd_sicslowpan_ctx_id_set_add: External Route unknown.\n\r");
+
+		if ( thrd_get_num_sicslowpan_ctx_ids() == THRD_MAX_SICSLOWPAN_CTX_ID_SET_ENTRIES ) {
+			/* Removing the last entry from the 6LoWPAN Context ID Set. */
+			thrd_sicslowpan_ctx_id_set_t *last;
+
+			last = list_tail(sicslowpan_ctx_id_list);
+			thrd_sicslowpan_ctx_id_set_rm(last);
+		}
+
+		/* Allocate a router id entry and populate it. */
+		ctx_id = memb_alloc(&sicslowpan_ctx_id_memb);
+
+		if ( ctx_id == NULL ) {
+			/* This should not happen, as we explicitly deallocated one
+			 * link set entry above. */
+			PRINTF("thrd_sicslowpan_ctx_id_set_add: Could not allocate 6LoWPAN Context ID.\n\r");
+			return NULL;
+		}
+		list_push(ext_route_list, ctx_id);
+
+		ctx_id->CID_id = id;
+		memcpy(&(ctx_id->CID_prefix[0]), prefix, 8);
+		ctx_id->CID_compress = compress;
+		ctx_id->CID_stable = stable;
+		PRINTF("thrd_sicslowpan_ctx_id_set_add: Added 6LoWPAN Context ID.\n\r");
+		num_sicslowpan_ctx_ids++;
+		PRINTF("thrd_sicslowpan_ctx_id_set_add: num_sicslowpan_ctx_ids %d\n\r", num_sicslowpan_ctx_ids);
+	} else {
+
+		PRINTF(ANSI_COLOR_RED "thrd_sicslowpan_ctx_id_set_add: 6LoWPAN Context ID is already known.\n\r");
+		PRINTF("thrd_sicslowpan_ctx_id_set_add: num_sicslowpan_ctx_ids %d\n\r", num_sicslowpan_ctx_ids);
+		PRINTF("-----------------------------------------------------\n\r");
+		return NULL;
+	}
+	PRINTF("-----------------------------------------------------\n\r");
+	return ctx_id;
+}
+
+/* --------------------------------------------------------------------------- */
+
+static thrd_server_set_t*
+thrd_server_set_add(uint16_t enterprise_number, uint8_t *service_data,
+		uint16_t server_16, uint8_t *server_data, bool stable, uint8_t id)
+{
+	thrd_server_set_t *server;
+	server = thrd_server_set_lookup(enterprise_number);
+
+	if ( server == NULL ) {
+		PRINTF("thrd_server_set_add: External Route unknown.\n\r");
+
+		if ( thrd_get_num_servers() == THRD_MAX_SERVER_SET_ENTRIES ) {
+			/* Removing the last entry from the 6LoWPAN Context ID Set. */
+			thrd_server_set_t *last;
+
+			last = list_tail(server_list);
+			thrd_server_set_rm(last);
+		}
+
+		/* Allocate a router id entry and populate it. */
+		server = memb_alloc(&server_memb);
+
+		if ( server == NULL ) {
+			/* This should not happen, as we explicitly deallocated one
+			 * link set entry above. */
+			PRINTF("thrd_server_set_add: Could not allocate 6LoWPAN Context ID.\n\r");
+			return NULL;
+		}
+		list_push(ext_route_list, server);
+
+		server->S_enterprise_number = enterprise_number;
+		memcpy(&(server->S_service_data[0]), service_data, 8);
+		server->S_server_16 = server_16;
+		memcpy(&(server->S_server_data[0]), server_data, 1);		// TODO Length of server data?
+		server->S_stable = stable;
+		server->S_id = id;
+		PRINTF("thrd_server_set_add: Added 6LoWPAN Context ID.\n\r");
+		num_servers++;
+		PRINTF("thrd_server_set_add: num_servers %d\n\r", num_servers);
+	} else {
+
+		PRINTF(ANSI_COLOR_RED "thrd_server_set_add: 6LoWPAN Context ID is already known.\n\r");
+		PRINTF("thrd_server_set_add: num_servers %d\n\r", num_servers);
+		PRINTF("-----------------------------------------------------\n\r");
+		return NULL;
+	}
+	PRINTF("-----------------------------------------------------\n\r");
+	return server;
+}
+
+/* --------------------------------------------------------------------------- */
+
 static void
 thrd_on_mesh_prefix_set_rm(thrd_on_mesh_prefix_set_t *pref)
 {
 	if ( pref != NULL ) {
 		PRINTF("thrd_on_mesh_prefix_set_rm: Removing On-Mesh Prefix from On-Mesh Prefix Set.\n\r");
-
-		/* Remove the router id from the Router ID Set. */
 		list_remove(on_mesh_prefix_list, pref);
 		memb_free(&on_mesh_prefix_memb, pref);
 		num_on_mesh_prefixes--;
@@ -416,10 +573,45 @@ thrd_on_mesh_prefix_set_rm(thrd_on_mesh_prefix_set_t *pref)
 
 /* --------------------------------------------------------------------------- */
 
-/* --------------------------------------------------------------------------- */
+static void
+thrd_ext_route_set_rm(thrd_ext_route_set_t *ext_route)
+{
+	if ( ext_route != NULL ) {
+		PRINTF("thrd_ext_route_set_rm: Removing External Route from External Route Set.\n\r");
+		list_remove(ext_route_list, ext_route);
+		memb_free(&ext_route_memb, ext_route);
+		num_ext_routes--;
+		PRINTF("thrd_ext_route_set_rm: num_ext_routes %d\n\r", num_ext_routes);
+	}
+}
 
 /* --------------------------------------------------------------------------- */
 
+static void
+thrd_sicslowpan_ctx_id_set_rm(thrd_sicslowpan_ctx_id_set_t *ctx_id)
+{
+	if ( ctx_id != NULL ) {
+		PRINTF("thrd_sicslowpan_ctx_id_set_rm: Removing 6LoWPAN Context ID from 6LoWPAN Context ID Set.\n\r");
+		list_remove(sicslowpan_ctx_id_list, ctx_id);
+		memb_free(&sicslowpan_ctx_id_memb, ctx_id);
+		num_sicslowpan_ctx_ids--;
+		PRINTF("thrd_sicslowpan_ctx_id_set_rm: num_sicslowpan_ctx_ids %d\n\r", num_sicslowpan_ctx_ids);
+	}
+}
+
+/* --------------------------------------------------------------------------- */
+
+static void
+thrd_server_set_rm(thrd_server_set_t *server)
+{
+	if ( server != NULL ) {
+		PRINTF("thrd_server_set_rm: Removing Server from Server Set.\n\r");
+		list_remove(server_list, server);
+		memb_free(&server_memb, server);
+		num_servers--;
+		PRINTF("thrd_server_set_rm: num_servers %d\n\r", num_servers);
+	}
+}
 
 /*
  ********************************************************************************
