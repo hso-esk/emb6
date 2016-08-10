c5b6e013048fc6d16476f9c9c19c6e046c0e8cff
mnguyen <minh.nguyen@hs-offenburg.de>
2016-08-10 17:02:17 +0200

    fixed missing packets in WOR mode for moses_ targets

--- target/if/cc112x/cc112x_cfg.h
+++ target/if/cc112x/cc112x_cfg.h
@@ -118,7 +118,7 @@ static const s_regSettings_t cc112x_cfg_ieee802154g_default[] =
     {CC112X_SYMBOL_RATE1,       0x99},
     {CC112X_SYMBOL_RATE0,       0x99},
 
-    {CC112X_AGC_REF,            0x3C},
+    {CC112X_AGC_REF,            0x3E},
     {CC112X_AGC_CS_THR,         0x0C},  /* default 0C -90dBm; 0x2A = -60dBm; 0x16 = -0x80dBm */
     {CC112X_AGC_CFG1,           0xA0},
     {CC112X_AGC_CFG0,           0xC0},  /* RSSI_VALID_CNT = 00b; 0x02 */
