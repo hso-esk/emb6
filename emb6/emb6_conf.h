3adc2d07cc5eb10bef815441e51a54231cf95061
lzimmer1 <lzimmer1@stud.hs-offenburg.de>
2016-11-14 15:43:55 +0100

    RPL probing: make delay function user-configurable
    6a5f238255a3a3450db2588cba7383d1be8529a0

--- emb6/emb6_conf.h
+++ emb6/emb6_conf.h
@@ -380,6 +380,16 @@
 #define RPL_PROBING_SEND_FUNC(instance, addr) dio_output((instance), (addr))
 #endif
 
+/*
+ * Function used to calculate next RPL probing interval
+ * */
+#ifdef RPL_CONF_PROBING_DELAY_FUNC
+#define RPL_PROBING_DELAY_FUNC RPL_CONF_PROBING_DELAY_FUNC
+#else
+#define RPL_PROBING_DELAY_FUNC() ((RPL_PROBING_INTERVAL / 2) \
+    + random_rand() % (RPL_PROBING_INTERVAL))
+#endif
+
 /*=============================================================================
                                   uIP SECTION
 ===============================================================================*/
