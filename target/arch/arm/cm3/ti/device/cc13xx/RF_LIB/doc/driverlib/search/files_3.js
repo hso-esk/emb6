d5080b67c46dd862de02b43281a5e911505fa368
mnidhal <mnidhal>
2017-03-20 15:09:22 +0100

    remove unnecessary files

--- target/arch/arm/cm3/ti/device/cc13xx/RF_LIB/doc/driverlib/search/files_3.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['event_2ec',['event.c',['../event_8c.html',1,'']]],
-  ['event_2edox',['event.dox',['../event_8dox.html',1,'']]],
-  ['event_2eh',['event.h',['../event_8h.html',1,'']]]
-];
