7e6d23f7586425764df41a2c725f12c499d709fc
nmars <nidhal.mars@hs-offenburg.de>
2017-05-15 16:19:47 +0200

    add tsch adaptive layer ( instead of dllsec_null)

---
+++ emb6/src/dll/tsch/tsch_null_adaptive.c
@@ -0,0 +1,67 @@
+#include "tsch_null_adaptive.h"
+#include "packetbuf.h"
+
+static s_ns_t *pdllsec_netstk;
+
+/*---------------------------------------------------------------------------*/
+static void init(s_ns_t *p_netstk)
+{
+#if NETSTK_CFG_ARG_CHK_EN
+  if (p_netstk == NULL) {
+    return;
+  }
+#endif
+
+  pdllsec_netstk = p_netstk;
+  //pdllsec_netstk->dllc->ioctrl(NETSTK_CMD_RX_CBFNT_SET, (void *) dllsec_input, &err);
+}
+/*---------------------------------------------------------------------------*/
+static void
+send(mac_callback_t sent, void *ptr)
+{
+
+  packetbuf_set_attr(PACKETBUF_ATTR_FRAME_TYPE, FRAME802154_DATAFRAME);
+
+  /*
+   * Issue next lower layer to transmit the prepared packet
+   */
+//  pdllsec_netstk->dllc->send(sent, ptr);
+  //NETSTACK_MAC.send(sent, ptr);
+
+
+  /* inform upper layer of the TX status */
+ // dllsec_cbTx(p_arg, &err);
+
+/****************************/
+
+}
+/*---------------------------------------------------------------------------*/
+static void input(void)
+{
+   pdllsec_netstk->hc->input();
+}
+
+/*---------------------------------------------------------------------------*/
+static int dllsec_onFrameCreated(void)
+{
+  return 1;
+}
+
+/*---------------------------------------------------------------------------*/
+static uint8_t dllsec_getOverhead(void)
+{
+  return 0;
+}
+
+/*---------------------------------------------------------------------------*/
+const struct s_nsDllsec dllsec_tsch_adaptive_driver = {
+  "nullsec",
+  init,
+  send,
+  input,
+  dllsec_onFrameCreated,
+  dllsec_getOverhead
+};
+/*---------------------------------------------------------------------------*/
+
+/** @} */
