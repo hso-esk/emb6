907fc413c998fcd0967e468c069332af81437647
nmars <nidhal.mars@hs-offenburg.de>
2017-07-17 14:06:50 +0200

    implement rtimer_arch_init fct

--- target/arch/arm/cm3/silabs/device/common/src/rtimer_arch.c
+++ target/arch/arm/cm3/silabs/device/common/src/rtimer_arch.c
@@ -7,17 +7,48 @@
                             INCLUDE FILES
 ==============================================================================*/
 #include "rtimer_arch.h"
-
-/*==============================================================================
-                          LOCAL FUNCTION PROTOTYPES
-==============================================================================*/
-void RTCIntHandler(void);
+#include "em_rtc.h"
+#include "em_device.h"
+#include "em_cmu.h"
+#include "em_common.h"
+#include "em_int.h"
 
 /*============================================================================*/
 /* rtimer_arch_init() */
 /*============================================================================*/
+static const RTC_Init_TypeDef initRTC =
+{
+  true,  // Start counting when init completed.
+  false, // Disable updating RTC during debug halt.
+  false  // Count until max. to wrap around.
+};
+
 void rtimer_arch_init(void)
 {
+  // Ensure LE modules are clocked.
+  CMU_ClockEnable( cmuClock_CORELE, true );
+
+  // Enable LFACLK in CMU (will also enable oscillator if not enabled).
+  CMU_ClockSelectSet( cmuClock_LFA, cmuSelect_LFXO );
+
+  // Set clock divider.
+  CMU_ClockDivSet( cmuClock_RTC, cmuClkDiv_1 );
+
+  // Enable RTC module clock.
+  CMU_ClockEnable( cmuClock_RTC, true );
+
+  // Initialize RTC.
+  RTC_Init( &initRTC );
+
+  // Disable RTC/RTCC interrupt generation.
+  RTC_IntDisable( _RTC_IF_MASK );
+  RTC_IntClear( _RTC_IF_MASK );
+
+  RTC_CounterReset();
+
+  // Clear and then enable RTC interrupts in NVIC.
+  NVIC_ClearPendingIRQ(RTC_IRQn);
+  NVIC_EnableIRQ(RTC_IRQn);
 
 }/* rtimer_arch_init() */
 
